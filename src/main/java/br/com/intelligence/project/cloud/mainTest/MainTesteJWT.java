1acef8267c8584e0befb3c7ab8a53ec99bb68475
Douglas-Bonelli <doulas.bonelli@gmail.com>
2022-01-19 09:26:41 -0300

    First commit

---
+++ src/main/java/br/com/intelligence/project/cloud/mainTest/MainTesteJWT.java
@@ -0,0 +1,11 @@
+package br.com.intelligence.project.cloud.mainTest;
+
+public class MainTesteJWT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste Java Message");
+
+	}
+
+}
