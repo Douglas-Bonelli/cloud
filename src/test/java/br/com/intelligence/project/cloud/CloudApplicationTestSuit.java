ef19bc77ace4e89c59bbf5073abef5974a0d3edb
Douglas-Bonelli <doulas.bonelli@gmail.com>
2022-01-19 14:03:54 -0300

    Testes JwtService

---
+++ src/test/java/br/com/intelligence/project/cloud/CloudApplicationTestSuit.java
@@ -0,0 +1,12 @@
+package br.com.intelligence.project.cloud;
+
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("br.com.intelligence.project.cloud.jwt")
+public class CloudApplicationTestSuit {
+
+}
--- src/test/java/br/com/intelligence/project/cloud/jwt/JwtServiceTest.java
+++ src/test/java/br/com/intelligence/project/cloud/jwt/JwtServiceTest.java
@@ -2,17 +2,53 @@ package br.com.intelligence.project.cloud.jwt;
 
 import org.junit.jupiter.api.Test;
 
+import io.jsonwebtoken.lang.Assert;
 
 public class JwtServiceTest {
 	
+	//Instancia Global
+	private JwtService service;
+
 	
 	@Test
-	public void geradorDeToken() {
+	public void deveGerarToken() {
 		
-		JwtService service = new JwtService();
-		String token = service.createToken("Orgao115","30");
-		System.out.println(token);
+		this.service = new JwtService();
+		String token = this.service.createToken("Orgao115","30");
+		Assert.hasText(token);
 		
+	}
+	
+	@Test
+	public void deveDecodificarObjetoToken() {
+		
+		String objeto = "Orgao115";
+		
+		this.service = new JwtService();
+		String token = this.service.createToken( objeto ,"30");
+		
+		Assert.isTrue( objeto.equals( this.service.getSubject(token) ) );	
+	}
+	
+	@Test
+	public void deveVerificarExpiracao() {
+		String objeto = "Orgao115";
+		this.service = new JwtService();
+		String token = this.service.createToken( objeto ,"30");
+		
+		Assert.isTrue( this.service.isValid(token));
+	}
+	
+	
+	
+	@Test
+	public void naoDevePermitirTokenExpirado() {
+		
+		String objeto = "Orgao115";
+		this.service = new JwtService();
+		String token = this.service.createToken( objeto ,"0");
+		
+		Assert.isTrue( !this.service.isValid(token));	
 	}
 	
 
